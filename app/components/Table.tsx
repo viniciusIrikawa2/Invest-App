817921428bce8069593cdb87da9b6dfb26c46d40
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-07-09 16:03:16 -0300

    style: wallet page responsiveness

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -6,11 +6,11 @@ const Table = () => {
     const { formData } = useContext(AppContext);
     
     return (
-        <div className='mt-10 flex items-center justify-center'>
+        <div className='mt-10 flex items-center justify-center px-3'>
             {formData.length === 0 ? (
                 <span className='text-gray-500 text-center'> No data here. </span>
             ) : (
-                <table className='text-in-white w-1/2'>
+                <table className='text-in-white md:w-1/2 w-full '>
                     <thead>
                         <tr>
                             <th className='mx-3 text-xs text-start text-[#909090]'> Owner </th>
