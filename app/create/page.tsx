77a5438f76750bac2b468a12d9067aa320b8e97f
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-07-09 09:29:52 -0300

    feat: field validation

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,7 +1,15 @@
+'use client';
 import React from 'react';
 import { today } from '../constants/constants';
 
 const CreatePage = () => {
+
+  const handleInputNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const sanitizedValue = value.replace(/[^0-9]/g, '');
+    e.target.value = sanitizedValue;
+  };
+
   return (
     <main>
       <h1 className='mt-14 ml-10 text-in-white text-xl uppercase font-bold'> Create your investment </h1>
@@ -14,7 +22,7 @@ const CreatePage = () => {
           <input className='text-black mb-5 rounded-sm p-2' type="date" id='creation-date' max={today} required/>
 
           <label htmlFor="initial-value" className='text-gray-400 mb-1 text-xs'> Initial value </label>
-          <input className='text-black mb-5 rounded-sm p-2' type="number" id='initial-value' required/>
+          <input className='text-black mb-5 rounded-sm p-2' type="text" id='initial-value' onChange={handleInputNumberChange} pattern="\d*" required/>
 
           <button className='bg-[#4ade80] text-[#2f2f2f] font-bold py-3 rounded-md uppercase'> Create </button>
         </form>
