c9a62c2d9107dc26e157252a287204f772478925
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-07-09 16:00:58 -0300

    style: navbar responsiveness

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 const NavBar = () => {
   return (
     <header>
-        <nav className='p-8 bg-[#2f2f2f] border-l-4 border-green-400 flex items-center justify-between'>
-            <h1 className='text-xl text-in-white font-bold'> 
+        <nav className='md:p-8 p-3 bg-[#2f2f2f] border-l-4 border-green-400 flex md:flex-row flex-col items-center justify-between'>
+            <h1 className='text-xl text-in-white font-bold md:mb-0 mb-5'> 
                 <Link href='/'>
                     <span className='text-green-400 text-2xl'>IN</span>vest<span className='text-green-400'>.</span>
                 </Link>
             </h1>
-            <ul className='uppercase flex items-center justify-around text-in-white w-1/3 text-sm cursor-pointer'>
+            <ul className='uppercase flex items-center justify-around text-in-white md:w-1/3 w-full text-sm cursor-pointer'>
                 <li>
                     <Link href=''>
                         our team 
--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useContext } from 'react';
+import React from 'react';
 import Table from '../components/Table';
 
 const WalletPage = () => {
