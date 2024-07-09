ce73cfac024cf05db74fd9a1b86fd950d0962687
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-07-09 15:42:58 -0300

    feat: listing investment in wallet page

---
+++ app/components/Table.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/InvestmentContext';
+import { IInvestment } from '../@Types/Investment';
+
+const Table = () => {
+    const { formData } = useContext(AppContext);
+    
+    return (
+        <div className='mt-10 flex items-center justify-center'>
+            {formData.length === 0 ? (
+                <span className='text-gray-500 text-center'> No data here. </span>
+            ) : (
+                <table className='text-in-white w-1/2'>
+                    <thead>
+                        <tr>
+                            <th className='mx-3 text-xs text-start text-[#909090]'> Owner </th>
+                            <th className='mx-3 text-xs text-start text-[#909090]'> Creation Date </th>
+                            <th className='mx-3 text-xs text-start text-[#909090]'> Initial Value </th>
+                            <th className='mx-3 text-xs text-start text-[#909090]'> Action </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {formData.map((item: IInvestment, index: number) => (
+                            <tr key={index}>
+                                <td> {item.owner} </td>
+                                <td> {item.creationDate} </td>
+                                <td> ${item.initialValue.toFixed(2)} </td>
+                                <td> <button className='bg-gray-600 rounded-md px-2 py-2 text-xs'> view </button> </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    )
+}
+
+export default Table;
--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 import React, { useContext } from 'react';
-import { AppContext } from '../context/InvestmentContext';
+import Table from '../components/Table';
 
 const WalletPage = () => {
-    const { formData } = useContext(AppContext);
-    
+
     return (
         <div>
             <h1 className='mt-14 ml-10 text-in-white text-xl uppercase font-bold'> My Wallet </h1>
+            <Table/>
         </div>
     )
 }
