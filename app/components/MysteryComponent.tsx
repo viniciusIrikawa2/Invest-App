8eabc9414c8d0b32e5445ffba788e3b88713a082
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-07-10 10:13:00 -0300

    style: mystery component animation

--- app/components/MysteryComponent.tsx
+++ app/components/MysteryComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const MysteryComponent = () => {
   return (
-    <div className='w-[50px] h-[50px] bg-green-400 border-4 border-green-300 rounded-full flex items-center justify-center fixed bottom-10 right-10 cursor-pointer'>
+    <div className='w-[50px] h-[50px] bg-green-400 border-4 border-green-300 rounded-full flex items-center justify-center fixed bottom-10 right-10 cursor-pointer animate-pulse'>
         <Link target='_blank' 
               href='https://api.whatsapp.com/send?phone=+5579988545007&text=Fala%20Vinícius,%20gostei%20do%20seu%20projeto!'>
             <span className='text-[#212121] text-xl font-bold'>?</span>
